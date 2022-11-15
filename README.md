7725a5ba76db732428e439743aadd523cb4b954a
Rick Gigger <rick@gigger.net>
2022-11-14 20:31:07 -0700

    Initial commmit
    Basic code to set up a twitter API client using a bearer token
    Download profile info for each person you follow

---
+++ README.md
@@ -0,0 +1,9 @@
+## TODO
+
+- [x] Download profile info for all follows
+- [ ] Download all avatars
+- [ ] Create an HTML page to simulate their twitter profile
+- [ ] Categorize links by which one's have RSS feeds or are for specific sites (github, linktree, etc)
+- [ ] Find a way to save a prioritization
+- [ ] Export bookmarks
+- [ ] Twitter timeline to RSS
---
+++ src/export-folllows.ts
@@ -0,0 +1,51 @@
+import * as fs from 'fs';
+import { TwitterApi } from 'twitter-api-v2';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+if (typeof process.env.BEARER_TOKEN === 'undefined') {
+    console.log(`The environmental variable "BEARER_TOKEN" must be set for this script to work`);
+    process.abort();
+}
+
+if (!process.env.USER_NAME) {
+    console.log(`The environmental variable "USER_NAME" must be set for this script to work`);
+    process.abort();
+}
+
+const bearerToken = process.env.BEARER_TOKEN;
+const username = process.env.USER_NAME;
+
+async function main() {
+    // Instantiate with desired auth type (here's Bearer v2 auth)
+    const twitterClient = new TwitterApi(bearerToken);
+    const fullClient = twitterClient.readOnly;
+    const client = fullClient.v2;
+
+    const user = await client.userByUsername(username);
+    console.log({user});
+
+    // 'created_at' | 'description' | 'entities' | 'id' | 'location' | 'name' | 'pinned_tweet_id' |
+    // 'profile_image_url' | 'protected' | 'public_metrics' | 'url' | 'username' | 'verified' | 'withheld'
+
+    const followees = await client.following(user.data.id, {
+        asPaginator: true,
+        max_results: 1000,
+        "user.fields": ['description', 'entities', 'id', 'name', 'profile_image_url', 'protected', 'url', 'username', 'verified', 'withheld'],
+    });
+
+    let count = 0;
+    const allFollowees: any[] = [];
+    for await (const followee of followees) {
+        allFollowees.push(followee);
+        count++;
+    }
+    console.log(`${count} followees found`);
+
+    const jsonContents: string = JSON.stringify(allFollowees, null, 4);
+    fs.writeFileSync(`follows-${(new Date()).getTime()}`, jsonContents);
+}
+
+
+main().catch(console.error);
